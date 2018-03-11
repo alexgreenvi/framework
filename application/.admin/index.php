434396acbc6a8950653b1ea2c7b00ffa25b12786
Если чё я Леха <alexgreenvi@yandex.ru>
2018-03-11 17:40:14 +0300

    Верстка

--- application/.admin/index.php
+++ application/.admin/index.php
@@ -5,32 +5,8 @@
                 <div class="aside__menu">
                     <div class="aside__menu__title">
                         <span>Модули</span>
-                        <div class="aside__menu__title__add">
-                            <a href="#popup_module_add" data-fancybox="" class="core__icon core__icon_plus"></a>
-                        </div>
-
-<!--                        <div class="popup" id="popup_module_add">-->
-<!--                            <div class="sh-form">-->
-<!--                                <div class="sh-form__field">-->
-<!--                                    <div class="sh-form__field__name">-->
-<!--                                        <span>Название</span>-->
-<!--                                    </div>-->
-<!--                                    <div class="sh-input">-->
-<!--                                        <input class="sh-input__control"/>-->
-<!--                                    </div>-->
-<!--                                </div>-->
-<!--                                <div class="sh-form__field">-->
-<!--                                    <div class="sh-form__field__name">-->
-<!--                                        <span>code</span>-->
-<!--                                    </div>-->
-<!--                                    <div class="sh-input">-->
-<!--                                        <input class="sh-input__control"/>-->
-<!--                                    </div>-->
-<!--                                </div>-->
-<!--                                <div class="sh-form__field">-->
-<!--                                    <button class="sh-btn">Добавить</button>-->
-<!--                                </div>-->
-<!--                            </div>-->
+<!--                        <div class="aside__menu__title__add">-->
+<!--                            <a href="#popup_module_add" data-fancybox="" class="core__icon core__icon_plus"></a>-->
 <!--                        </div>-->
                     </div>
                     <ul class="aside__menu__list">
